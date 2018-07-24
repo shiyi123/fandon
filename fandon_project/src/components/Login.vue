f87a360f06b4924b58fc0e325d421345f8121889
lily <lily@tsingoal.com>
2018-07-24 14:30:12 +0800

    first c

---
+++ fandon_project/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+<mt-header class="header" fixed title="">
+  <mt-button  class="jump" slot="right">跳过</mt-button>
+</mt-header>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+     msg :"333"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  background:#42b983
+}
+.jump{
+  color:#000
+}
+</style>
---
+++ fandon_project/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { Header,Button } from 'mint-ui';
+Vue.component(Header.name, Header);
+Vue.component(Button.name, Button);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
